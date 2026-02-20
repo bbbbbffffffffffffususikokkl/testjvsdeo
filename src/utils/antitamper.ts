2fd1d0bc725ed95c6242bee3b1dcfdf1b7d8e6ae
polnoref <yupiooipyupio@gmail.com>
2026-02-20 15:35:23 +0500

    Add files via upload

--- src/utils/antitamper.ts
+++ src/utils/antitamper.ts
@@ -1,1 +1,7 @@
-
+/**
+ * utils/antitamper.ts
+ *
+ * Re-exports the AntiTamperRemover transformation so it can be imported
+ * from the short path `src/utils/antitamper` as requested.
+ */
+export { AntiTamperRemover } from '../deobfuscator/antiTamperRemover';
