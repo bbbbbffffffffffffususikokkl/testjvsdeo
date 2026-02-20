d12c1143d2812f104637bd8451354904d731a076
polnoref <yupiooipyupio@gmail.com>
2026-02-20 15:32:25 +0500

    Delete src directory

--- src/utils/memberexpressionfixer.js
+++
@@ -1,4 +0,0 @@
-export function fixMemberExpressions(code) {
-    // Converts object['property'] to object.property
-    return code.replace(/([a-zA-Z0-9_$]+)\[['"]([a-zA-Z_$][a-zA-Z0-9_$]*)['"]\]/g, '$1.$2');
-}
