3dacc82285df473da7b87e75a69752efad140932
polnoref <yupiooipyupio@gmail.com>
2026-02-20 12:05:34 +0500

    Add files via upload

--- src/engine.js
+++ src/engine.js
@@ -1,5 +1,6 @@
 import { decodeHex } from './utils/hexdecoder.js';
 import { removeJunk } from './utils/junkremover.js';
+import { unpackStrings } from './utils/stringarrayunpacker.js'; // NEW
 import { renameVariables } from './utils/renamer.js';
 import { addWatermark } from './utils/watermark.js';
 
@@ -7,18 +8,11 @@ export function deobfuscate(code, startTime) {
     let output = code;
 
     try {
-        // 1. Decode Hex (Crucial for MCBE imports)
         output = decodeHex(output);
-
-        // 2. Remove Junk (Prevents logic bloat)
+        output = unpackStrings(output); // Run this now!
         output = removeJunk(output);
-
-        // 3. Rename (Fixes the _0x names)
         output = renameVariables(output);
-
-        // 4. Add Vex Watermark
         output = addWatermark(output, startTime);
-
     } catch (e) {
         output = `// Deobfuscation Error: ${e.message}\n\n` + output;
     }
