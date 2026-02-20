883dc1aeba661cb06e1a6a0de9bde847b9b074c7
polnoref <yupiooipyupio@gmail.com>
2026-02-20 12:42:59 +0500

    Add files via upload

--- src/engine.js
+++ src/engine.js
@@ -1,8 +1,8 @@
 import { decodeHex } from './utils/hexdecoder.js';
-import { removeJunk } from './utils/junkremover.js';
-import { unpackStrings } from './utils/stringarrayunpacker.js'; // NEW
-import { fixMemberExpressions } from "./utils/memberexpressionfixer.js"
+import { unpackStrings } from './utils/stringarrayunpacker.js';
 import { renameVariables } from './utils/renamer.js';
+import { removeJunk } from './utils/junkremover.js';
+import { beautify } from './utils/formatter.js';
 import { addWatermark } from './utils/watermark.js';
 
 export function deobfuscate(code, startTime) {
@@ -10,13 +10,22 @@ export function deobfuscate(code, startTime) {
 
     try {
         output = decodeHex(output);
-        output = unpackStrings(output); // Run this now!
-        output = fixMemberExpressions(output);
+        output = unpackStrings(output);
+        
+        // This is where the magic happens:
+        // Turns _0x55d1 -> world, _0x8821 -> player, etc.
+        output = renameVariables(output); 
+
         output = removeJunk(output);
-        output = renameVariables(output);
+        
+        // Member Expression Fixer (Clean up those brackets)
+        output = output.replace(/\[['"]([^'"]+)['"]\]/g, '.$1'); 
+
+        output = beautify(output);
         output = addWatermark(output, startTime);
+
     } catch (e) {
-        output = `// Deobfuscation Error: ${e.message}\n\n` + output;
+        output = `// Deobfuscation Error: ${e.message}\n` + output;
     }
 
     return output;
