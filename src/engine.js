257543cd035aa5960421b79dc349d7fd26b7719c
polnoref <yupiooipyupio@gmail.com>
2026-02-21 21:47:32 +0500

    Add files via upload

--- src/engine.js
+++ src/engine.js
@@ -11,34 +11,18 @@ export function deobfuscate(code, startTime) {
     let output = code;
 
     try {
-        // 1. Basic cleaning: Hex -> String
         output = decodeHex(output);
-
-        // 2. Resolve math (Obfuscator.io parseInt/arithmetic support)
-        // MUST happen before unpacking so indices are numbers
         output = simplifyMath(output); 
-
-        // 3. String Array Unpacking (The ben-sb / simulator logic)
+        output = decodeHex(output);
         output = unpackStrings(output);
-
-        // 4. Rename MCBE variables (e.g., _0x55d1 -> world)
         output = renameVariables(output);
-
-        // 5. Member Fixer (console['warn'] -> console.warn)
         output = output.replace(/([a-zA-Z0-9_$]+)\[['"]([^'"]+)['"]\]/g, '$1.$2');
-
-        // 6. Aggressive Junk Removal (Delete unused var/functions)
         output = removeJunk(output);
         output = refineDecals(output);
-
-        // 7. Formatting (Final visual polish)
         output = beautify(output);
-
-        // 8. Add Watermark at the top
         output = addWatermark(output, startTime);
-
     } catch (e) {
-        output = `// Vex Deobfuscator Error: ${e.message}\n` + output;
+        output = `// Deobfuscator Error: ${e.message}\n` + output;
     }
 
     return output;
